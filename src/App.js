8428b409b6334e8c1c8d1a9de1ff5a13474e5638
Scott Geleas <scottgeleas97@gmail.com>
2022-02-17 15:20:47 -0500

    removed eslint, added folder structure and header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
-    return <>HELLO</>;
+    return (
+        <>
+            <Header />
+        </>
+    );
 }
 
 export default App;

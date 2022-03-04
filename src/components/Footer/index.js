0b2485982facef652cc92649da9945b7539a80fc
Chris <christangarife95@gmail.com>
2022-03-04 18:00:05 -0500

    fixed certification positioning and added media queries

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import './style.css';
 import issa from '../../images/trainer-certification.png';
+import { Link } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
 
 export default function Footer() {
@@ -10,14 +11,15 @@ export default function Footer() {
             <Container fluid id="footer">
                 <Row>
                     <Col xxl={8} className="contact">
+                        <Container>
                         <h3>Questions? Or want to book a free consultation?</h3>
                         <p id='num'>
                             {/* Call me at (505)-555-5555 or send me an emal through
                             the <a>Contact</a> page and I will get back to you
                             as soon as possible. */}
-                            Send me an email through the <LinkContainer to='/contact'><Button >contact</Button></LinkContainer> page or call me at <LinkContainer>(603)-425-9064</LinkContainer>
+                            Send me an email through the <Link to='/contact'>contact</Link> page or call me at <a id='phone' href='tel:+1-603-425-9064'>(603)-425-9064</a>
                         </p>
-                       
+                       </Container>
                     </Col>
                 </Row>
                 

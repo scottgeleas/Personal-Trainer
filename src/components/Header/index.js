634b84df66dc3c594edba5e43c9d943da8cb2578
Scott Geleas <scottgeleas97@gmail.com>
2022-04-06 17:04:34 -0400

    code formatting, removed unused styles and comments

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
-import { Navbar, Nav, Container, Image, Col, Stack } from 'react-bootstrap';
+import { Navbar, Nav, Image, Col, Stack } from 'react-bootstrap';
 import './index.css';
 import dumbbell from '../../images/dumbbell.png';
 
 export default function Header() {
     return (
         <Navbar id="navbar" expand="lg">
-            <Col className='col'>
-                <Image id="logo" src={dumbbell} className="align-top m-auto" />
+            <Col className="col">
+                <Image id="logo" className="align-top m-auto" src={dumbbell} />
             </Col>
-            <Col className='col'>
+            <Col className="col">
                 <LinkContainer to="/">
                     <Navbar.Brand id="brand-name" className="m-auto">
                         {' '}
@@ -18,62 +18,24 @@ export default function Header() {
                     </Navbar.Brand>
                 </LinkContainer>
             </Col>
-            <Col id='navigation' className='col'>
-                <Navbar.Toggle className="m-auto" aria-controls="basic-navbar-nav" />
+            <Col className="col">
+                <Navbar.Toggle
+                    className="m-auto"
+                    aria-controls="basic-navbar-nav"
+                />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="m-auto" id="links">
-                    <Stack direction='horizontal' gap={4}>
-                        <LinkContainer to="/" className="nav-link">
-                            <Nav.Link>About</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/contact" className="nav-link">
-                            <Nav.Link>Contact</Nav.Link>
-                        </LinkContainer>
+                    <Nav className="m-auto">
+                        <Stack direction="horizontal" gap={4}>
+                            <LinkContainer to="/" className="nav-link">
+                                <Nav.Link>About</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/contact" className="nav-link">
+                                <Nav.Link>Contact</Nav.Link>
+                            </LinkContainer>
                         </Stack>
                     </Nav>
                 </Navbar.Collapse>
             </Col>
         </Navbar>
-
-        // <Navbar id="navbar">
-        //      <Container id="navigation">
-        //          <LinkContainer to="/">
-        //              <Container id="brand">
-        //                  <Image id="logo" src={dumbbell} className='align-top'/>
-        //                  <Navbar.Brand id="brand-name">
-        //                      Dylan Clifford
-        //                  </Navbar.Brand>
-        //              </Container>
-        //          </LinkContainer>
-        //          <Navbar.Toggle id='collapse' aria-controls="basic-navbar-nav" />
-        //          <Navbar.Collapse id="basic-navbar-nav">
-        //              <Nav className="ms-auto">
-        //              {/* <Stack direction="horizontal" gap={5}> */}
-        //                  <LinkContainer to="/" className="nav-link">
-        //                      <Nav.Link>About</Nav.Link>
-        //                  </LinkContainer>
-        //                  <LinkContainer to="/contact" className="nav-link">
-        //                      <Nav.Link>Contact</Nav.Link>
-        //                  </LinkContainer>
-        //                  {/* </Stack> */}
-        //              </Nav>
-        //          </Navbar.Collapse>
-        //      </Container>
-        //  </Navbar>
-
-        // {/* <Navbar expand='xl' id='navbar'>
-        //     <Container>
-        //       <Navbar.Brand href="#home">
-        //         <img
-        //           alt=""
-        //           src={dumbbell}
-        //           width="30"
-        //           height="30"
-        //           className="d-inline-block align-top"
-        //         />{' '}
-        //       Dylan Clifford
-        //       </Navbar.Brand>
-        //     </Container>
-        //   </Navbar> */}
     );
 }

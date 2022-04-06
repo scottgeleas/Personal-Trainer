d0ba23441dbf64c44627ee2f568210633431ef6b
Chris <christangarife95@gmail.com>
2022-04-06 16:08:17 -0400

    final format for the footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,33 +1,33 @@
 import React from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './style.css';
 import issa from '../../images/trainer-certification.png';
-import { Link } from 'react-router-dom';
-import { LinkContainer } from 'react-router-bootstrap';
 
 export default function Footer() {
     return (
         <footer>
-            <Container fluid id="footer">
+            <Container fluid id='footer'>
                 <Row>
                     <Col id='col'>
                         <Container id='information'>
-                        <h3>Questions? Or want to book a free consultation?</h3>
-                        <p>
-                            {/* Call me at (505)-555-5555 or send me an emal through
-                            the <a>Contact</a> page and I will get back to you
-                            as soon as possible. */}
-                            Send me an email through the <Link to='/contact'>contact</Link> page or call me at <a id='phone' href='tel:+1-603-425-9064'>(603)-425-9064</a>
-                        </p>
-                       </Container>
+                            <h3>
+                                Questions? Or want to book a free consultation?
+                            </h3>
+                            <p>
+                                Send me an email through the{' '}
+                                <Link to='/contact'>contact</Link> page or call
+                                me at{' '}
+                                <a id='phone' href='tel:+1-603-425-9064'>
+                                    (603)-425-9064
+                                </a>
+                            </p>
+                        </Container>
                     </Col>
                 </Row>
-                
 
-                 {/* <div className='vl'></div> */}
-                 
                 <Row>
-                    <Col  className="links">
+                    <Col className='links'>
                         <img src={issa} alt='issa-certification'></img>
                     </Col>
                 </Row>

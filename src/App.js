39157f7e21a5137cb6beab0f6461b39dee8cc9ad
Chris <christangarife95@gmail.com>
2022-04-12 15:55:00 -0400

    created packages component, added skeleton for card, added component to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Packages from './components/Packages';
 import Footer from './components/Footer';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -9,6 +10,7 @@ function App() {
         <>
             <Router>
             <Header />
+            <Packages/>
             <Footer />
                 <Routes>
                 <Route exact path="/" element={<h1>Home</h1>} />
---
+++ src/components/Packages/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {Card} from 'react-bootstrap';
+import './index.css';
+
+
+export default function Packages() {
+    return (
+        <Card>
+        <Card.Header>Header</Card.Header>
+        <Card.Body>
+          <Card.Title>Card Title </Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk
+            of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    );
+}

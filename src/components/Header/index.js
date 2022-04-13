375eaad073a447eb7941f9ea0ccc9b608edeb363
Scott Geleas <scottgeleas97@gmail.com>
2022-04-13 12:47:14 -0400

    changed format of header to make it simpler

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,53 +1,31 @@
 import React from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
-import { Navbar, Nav, Image, Container } from 'react-bootstrap';
-import './index.css';
+import { Navbar, Nav, Image, Col } from 'react-bootstrap';
+import './style.css';
 import dumbbell from '../../images/dumbbell.png';
 
 export default function Header() {
     return (
-        <Navbar id='navbar' expand="lg">
-         
-                <LinkContainer to="/">
-                    <Navbar.Brand id='brand-name'>
-                        <Image id="logo" src={dumbbell} /> Dylan Clifford
-                    </Navbar.Brand>
-                </LinkContainer>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="ms-auto">
-                        <LinkContainer to="/" className="nav-link">
-                            <Nav.Link>About</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/contact" className="nav-link">
+        <header>
+            <Navbar id="navbar">
+                <Col>
+                    <Image id="logo" src={dumbbell} />
+                </Col>
+                <Col>
+                    <LinkContainer to="/">
+                        <Navbar.Brand id="brand-name">
+                            Dylan Clifford
+                        </Navbar.Brand>
+                    </LinkContainer>
+                </Col>
+                <Col>
+                    <Nav>
+                        <LinkContainer to="#contact" id="nav-link">
                             <Nav.Link>Contact</Nav.Link>
                         </LinkContainer>
                     </Nav>
-                </Navbar.Collapse>
-           
-        </Navbar>
-        // <Navbar id="navbar" expand="lg">
-        //     <Container>
-        //         <Image id="logo" src={dumbbell} />
-        //     </Container>
-        //     <Container>
-        //         <LinkContainer to="/">
-        //             <Navbar.Brand id="brand-name">Dylan Clifford</Navbar.Brand>
-        //         </LinkContainer>
-        //     </Container>
-        //     <Container>
-        //         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        //         <Navbar.Collapse id="basic-navbar-nav">
-        //             <Nav>
-        //                 <LinkContainer to="/" className="nav-link">
-        //                     <Nav.Link>About</Nav.Link>
-        //                 </LinkContainer>
-        //                 <LinkContainer to="/contact" className="nav-link">
-        //                     <Nav.Link>Contact</Nav.Link>
-        //                 </LinkContainer>
-        //             </Nav>
-        //         </Navbar.Collapse>
-        //     </Container>
-        // </Navbar>
+                </Col>
+            </Navbar>
+        </header>
     );
 }

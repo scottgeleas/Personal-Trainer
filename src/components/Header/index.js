eb4cc40eb39cba6ea6c82028021128864d69f739
Scott Geleas <scottgeleas97@gmail.com>
2022-04-13 12:01:43 -0400

    changed style of header, looking for ways to make space-evenly work

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,41 +1,53 @@
 import React from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
-import { Navbar, Nav, Image, Col, Stack } from 'react-bootstrap';
+import { Navbar, Nav, Image, Container } from 'react-bootstrap';
 import './index.css';
 import dumbbell from '../../images/dumbbell.png';
 
 export default function Header() {
     return (
-        <Navbar id="navbar" expand="lg">
-            <Col className="col">
-                <Image id="logo" className="m-auto" src={dumbbell} />
-            </Col>
-            <Col className="col">
+        <Navbar id='navbar' expand="lg">
+         
                 <LinkContainer to="/">
-                    <Navbar.Brand id="brand-name" className="m-auto">
-                        {' '}
-                        Dylan Clifford
+                    <Navbar.Brand id='brand-name'>
+                        <Image id="logo" src={dumbbell} /> Dylan Clifford
                     </Navbar.Brand>
                 </LinkContainer>
-            </Col>
-            <Col className="col">
-                <Navbar.Toggle
-                    className="m-auto"
-                    aria-controls="basic-navbar-nav"
-                />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="m-auto">
-                        <Stack direction="horizontal" gap={4}>
-                            <LinkContainer to="/" className="nav-link">
-                                <Nav.Link>About</Nav.Link>
-                            </LinkContainer>
-                            <LinkContainer to="/contact" className="nav-link">
-                                <Nav.Link>Contact</Nav.Link>
-                            </LinkContainer>
-                        </Stack>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ms-auto">
+                        <LinkContainer to="/" className="nav-link">
+                            <Nav.Link>About</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/contact" className="nav-link">
+                            <Nav.Link>Contact</Nav.Link>
+                        </LinkContainer>
                     </Nav>
                 </Navbar.Collapse>
-            </Col>
+           
         </Navbar>
+        // <Navbar id="navbar" expand="lg">
+        //     <Container>
+        //         <Image id="logo" src={dumbbell} />
+        //     </Container>
+        //     <Container>
+        //         <LinkContainer to="/">
+        //             <Navbar.Brand id="brand-name">Dylan Clifford</Navbar.Brand>
+        //         </LinkContainer>
+        //     </Container>
+        //     <Container>
+        //         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        //         <Navbar.Collapse id="basic-navbar-nav">
+        //             <Nav>
+        //                 <LinkContainer to="/" className="nav-link">
+        //                     <Nav.Link>About</Nav.Link>
+        //                 </LinkContainer>
+        //                 <LinkContainer to="/contact" className="nav-link">
+        //                     <Nav.Link>Contact</Nav.Link>
+        //                 </LinkContainer>
+        //             </Nav>
+        //         </Navbar.Collapse>
+        //     </Container>
+        // </Navbar>
     );
 }

170edbb2b016c08a26d66aa9d679e5f2f992f975
Scott Geleas <scottgeleas97@gmail.com>
2022-04-14 15:28:05 -0400

    restructured files to include pages for home and contact

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
 
 function App() {
     return (
         <>
             <Router>
-            <Header />
-            <Footer />
                 <Routes>
-                <Route exact path="/" element={<h1>Home</h1>} />
-                <Route exact path="/contact" element={<h1>Contact</h1>}/>
+                    <Route exact path="/" element={<Home />} />
+                    <Route exact path="/contact" element={<Contact />} />
                 </Routes>
             </Router>
         </>

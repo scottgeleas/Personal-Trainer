dc99135d90a3a9f2ee23a007231b501dcddf64dd
Scott Geleas <scottgeleas97@gmail.com>
2022-04-14 16:56:37 -0400

    added basic form, fixed footer overlap issue

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './style.css';
-import Header from '../../components/Header'
-import Footer from '../../components/Footer'
+import Header from '../../components/Header';
+import ContactForm from '../../components/ContactForm';
+import Footer from '../../components/Footer';
 
 export default function Contact() {
     return (
-        <div>
+        <>
             <Header />
             <h1>Contact</h1>
+            <main>
+                <ContactForm />
+            </main>
             <Footer />
-        </div>
+        </>
     );
 }

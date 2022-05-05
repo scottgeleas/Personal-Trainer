638908e8271d0bc731dce81cd0c0fd4ae0424b84
Chris <christangarife95@gmail.com>
2022-05-05 12:44:05 -0400

    Card styling and placement

--- src/components/Package/index.js
+++ src/components/Package/index.js
@@ -1,20 +1,21 @@
 import React from "react";
-import { Card } from 'react-bootstrap';
+import { Card, Container } from 'react-bootstrap';
 import './styles.css';
 
 
 export default function Package({ packageInfo }) {
     return (
+        <Container>
 
-        <Card>
-            <Card.Title>{packageInfo.title}</Card.Title>
-            <Card.Subtitle>{packageInfo.rate}</Card.Subtitle>
-            <Card.Body>
-                <Card.Text>{packageInfo.description}</Card.Text>
-            </Card.Body>
-        </Card>
+            <Card style={{ width: "18rem" }}>
+                <Card.Title>{packageInfo.title}</Card.Title>
+                <Card.Subtitle>{packageInfo.rate}</Card.Subtitle>
+                <Card.Body>
+                    <Card.Text>{packageInfo.description}</Card.Text>
+                </Card.Body>
+            </Card>
 
-
+        </Container>
 
     );
 }

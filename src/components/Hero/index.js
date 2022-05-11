1844cb5b21a74cd7c6eb30e9dc34164e44d4a121
Chris <christangarife95@gmail.com>
2022-05-11 16:59:59 -0400

    created hero component and skeleton

---
+++ src/components/Hero/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { DEFAULT_BREAKPOINTS } from 'react-bootstrap/esm/ThemeProvider';
+
+export default function Hero() {
+    return (
+        <h1>hello</h1>
+    );
+}

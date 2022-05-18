c2f9efc812a655a4c102e52141bee375e24414fd
Scott Geleas <scottgeleas97@gmail.com>
2022-05-18 17:19:23 -0400

    contact page formatting, list sizing, breakpoints, BUG- width of row makes page not center items correctly

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -3,14 +3,41 @@ import './style.css';
 import Header from '../../components/Header';
 import ContactForm from '../../components/ContactForm';
 import Footer from '../../components/Footer';
+import { Row, Col, Image, ListGroup } from 'react-bootstrap';
+import issa_logo from '../../images/issa-logo.png';
 
 export default function Contact() {
     return (
         <>
             <Header />
-            <main>
-                <ContactForm />
-            </main>
+            <Row>
+                <Col id="examples" className="column" sm={12} lg={6}>
+                    <ListGroup id="examples-list" as="ul" variant="flush">
+                        <ListGroup.Item as="li" id="list-heading">
+                            Questions you can ask me:
+                        </ListGroup.Item>
+                        <ListGroup.Item as="li" className="list-item">
+                            How did the thing happen?
+                        </ListGroup.Item>
+                        <ListGroup.Item as="li" className="list-item">
+                            How did the thing happen?
+                        </ListGroup.Item>
+                        <ListGroup.Item as="li" className="list-item">
+                            How did the thing happen?
+                        </ListGroup.Item>
+                        <ListGroup.Item as="li" className="list-item">
+                            How did the thing happen?
+                        </ListGroup.Item>
+                        <ListGroup.Item as="li" className="list-item">
+                            How did the thing happen?
+                        </ListGroup.Item>
+                    </ListGroup>
+                    <Image id="issa-logo" src={issa_logo} />
+                </Col>
+                <Col id="contact-form" className="column" sm={12} lg={6}>
+                    <ContactForm />
+                </Col>
+            </Row>
             <Footer />
         </>
     );

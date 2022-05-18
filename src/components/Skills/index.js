73c34e4b29d50c6a3c0993e93807c437cd766a44
Chris <christangarife95@gmail.com>
2022-05-18 17:17:53 -0400

    added content to skills component, beginning to style

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,8 +1,11 @@
 import React from "react";
-
+import './style.css'
 
 export default function Skills () {
     return (
-        <h1>Skills section</h1>
+        <section className="skillsection">
+        <h2>Skills</h2>
+        <p className="skillinfo">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet nulla facilisi morbi tempus iaculis urna id. Arcu non sodales neque sodales ut etiam sit amet nisl. Ante metus dictum at tempor commodo ullamcorper a lacus vestibulum.</p>
+        </section>
     )
 }

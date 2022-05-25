63fc1127207683bc2f9a4bcb70e38ffbdff699a1
Chris <christangarife95@gmail.com>
2022-05-25 15:58:29 -0400

    switched all classes to id

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -4,8 +4,8 @@ import './style.css'
 export default function Skills () {
     return (
         <section>
-        <h2 className="skill-header">Skills</h2>
-        <p className="skill-info">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet nulla facilisi morbi tempus iaculis urna id. Arcu non sodales neque sodales ut etiam sit amet nisl. Ante metus dictum at tempor commodo ullamcorper a lacus vestibulum.</p>
+        <h2 id="skill-header">Skills</h2>
+        <p id="skill-info">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet nulla facilisi morbi tempus iaculis urna id. Arcu non sodales neque sodales ut etiam sit amet nisl. Ante metus dictum at tempor commodo ullamcorper a lacus vestibulum.</p>
         </section>
     )
 }

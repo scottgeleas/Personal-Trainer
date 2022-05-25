0cd1910eaa0a96773baf408ad6d13777611722bb
Scott Geleas <scottgeleas97@gmail.com>
2022-05-25 15:19:58 -0400

    fixed styling issues and bug with width of row

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Row, Col } from 'react-bootstrap';
-import './style.css';
 import issa from '../../images/trainer-certification.png';
+import './style.css';
 
 export default function Footer() {
     return (
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,43 +1,45 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap'
-import './style.css'
-import Header from '../../components/Header'
-import Package from '../../components/Package'
-import Footer from '../../components/Footer'
+import { Row, Col } from 'react-bootstrap';
+import Header from '../../components/Header';
+import Package from '../../components/Package';
+import Footer from '../../components/Footer';
+import './style.css';
 
 export default function Home() {
     const packages = {
-
         package1: {
             title: 'Package 1',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-            rate: 30
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+            rate: 30,
         },
         package2: {
             title: 'Package 2',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-            rate: 35
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+            rate: 35,
         },
         package3: {
             title: 'Package 3',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-            rate: 40
-        }
-    }
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+            rate: 40,
+        },
+    };
 
     return (
         <div>
             <Header />
-            <Row id='packages'>
-                <Col className='package-container'>
+            <Row id="packages">
+                <Col className="package-container">
                     <Package packageInfo={packages.package1} />
                 </Col>
 
-                <Col className='package-container'>
+                <Col className="package-container">
                     <Package packageInfo={packages.package2} />
                 </Col>
 
-                <Col className='package-container'>
+                <Col className="package-container">
                     <Package packageInfo={packages.package3} />
                 </Col>
             </Row>

346c029b40f8c99bd361d507695b3ef680ff1fe6
Scott Geleas <scottgeleas97@gmail.com>
2022-05-25 16:47:49 -0400

    changed header formating back to burger bar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
-import { Navbar, Nav, Image, Col } from 'react-bootstrap';
+import { Navbar, Nav, Image, Container, Button} from 'react-bootstrap';
 import dumbbell from '../../images/dumbbell.png';
 import './style.css';
 
 export default function Header() {
     return (
         <header>
-            <Navbar id="navbar" expand="lg" collapseOnSelect>
+            <Navbar expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Image id="logo" src={dumbbell} />
+                    <LinkContainer to="/">
+                        <Navbar.Brand id="brand-name">
+                            Dylan Clifford
+                        </Navbar.Brand>
+                    </LinkContainer>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto burger">
+                        <LinkContainer to="/" id="nav-link">
+                            <Nav.Link><Button>Home</Button></Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/contact" id="nav-link">
+                            <Nav.Link><Button>Contact</Button></Nav.Link>
+                        </LinkContainer>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            
+            {/* <Navbar id="navbar" expand="lg" collapseOnSelect>
                 <Col>
                     <Image id="logo" src={dumbbell} />
                 </Col>
@@ -32,7 +54,7 @@ export default function Header() {
                         </LinkContainer>
                     </Nav>
                 </Col>
-            </Navbar>
+            </Navbar> */}
         </header>
     );
 }

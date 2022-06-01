0cbe4560133954ebef782a841f759ab07daf4bfb
Scott Geleas <scottgeleas97@gmail.com>
2022-06-01 16:54:00 -0400

    added main button styles, added accent color, adjusting spacing between button element

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
-import { Navbar, Nav, Image, Container, Button} from 'react-bootstrap';
+import { Navbar, Nav, Image, Container} from 'react-bootstrap';
 import dumbbell from '../../images/dumbbell.png';
 import './style.css';
 
 export default function Header() {
     return (
         <header>
-            <Navbar expand="lg" bg="dark" variant="dark">
+            <Navbar id="navbar" expand="lg" bg="dark" variant="dark">
                 <Container>
                     <Image id="logo" src={dumbbell} />
                     <LinkContainer to="/">
@@ -18,17 +18,25 @@ export default function Header() {
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="ms-auto burger">
-                        <LinkContainer to="/" className="nav-link">
-                            <Nav.Link><Button>Home</Button></Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/contact" className="nav-link">
-                            <Nav.Link><Button>Contact</Button></Nav.Link>
-                        </LinkContainer>
+                            <LinkContainer to="/" className="nav-link">
+                                <Nav.Link>
+                                    <button id="first-link" className="link">
+                                        <span>Home</span>
+                                    </button>
+                                </Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/contact" className="nav-link">
+                                <Nav.Link>
+                                    <button className="link">
+                                        <span>Contact</span>
+                                    </button>
+                                </Nav.Link>
+                            </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-            
+
             {/* <Navbar id="navbar" expand="lg" collapseOnSelect>
                 <Col>
                     <Image id="logo" src={dumbbell} />

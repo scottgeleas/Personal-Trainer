1cfe615b814b14e5c5d62a646c7c2c2e2fcd5df6
Chris <72607953+ChrisCodes54@users.noreply.github.com>
2023-07-06 16:46:24 -0400

    fixed id naming convention

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -1,11 +1,10 @@
 import React from "react";
-import { Container } from 'react-bootstrap'
 import './style.css'
 
 export default function Avatar() {
     return (
-        <div id="something">
-            <img src="https://via.placeholder.com/500" id="avatar" />
+        <div id="avatarContainer">
+            <img src="https://via.placeholder.com/500" id="avatar" alt="" />
 
         </div>
     )
